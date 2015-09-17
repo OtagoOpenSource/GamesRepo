924ff2c130ebc5ac28ec842f05341f2ff382dfb5
lechszym <lechszym@gmail.com>
2015-09-17 22:59:57 +1200

    Trying to rotate avatar

--- Satellite/Assets/Scripts/PlanetoidGravity.cs
+++ Satellite/Assets/Scripts/PlanetoidGravity.cs
@@ -3,21 +3,28 @@ using System.Collections;
 
 public class PlanetoidGravity : MonoBehaviour {
 
+	Vector3 lastPosition;
+
 	// Use this for initialization
 	void Start () {
-	
+		lastPosition = transform.localPosition;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		//Rigidbody rb = GetComponent<Rigidbody>();
 		//Debug.Log (Physics.gravity.ToString ("F4"));
-		Vector3 newGravity = -transform.position;
+		Vector3 newGravity = -transform.localPosition;
 		newGravity = newGravity.normalized * 9.81f;
 		//diff.x = diff.x * 9.81f;
 		//diff.y = diff.y * 9.81f;
 		//diff.z = diff.z * 9.81f;
-		//Debug.Log (diff.ToString ("F4"));
+		//Debug.Log (newGravity.ToString("F4"));
 		Physics.gravity = newGravity;
+
+		transform.rotation = Quaternion.FromToRotation(lastPosition, transform.localPosition);
+
+		lastPosition = transform.localPosition;
+
 	}
 }
