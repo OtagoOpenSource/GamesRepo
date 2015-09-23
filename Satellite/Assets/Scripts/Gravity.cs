b29b576f37fbfd3046853ecccecd20b7d9b12a4e
Lech Szymanski <lechszym@gmail.com>
2015-09-23 22:00:53 +1200

    Preparing to add gravitational planetoid pull

---
+++ Satellite/Assets/Scripts/Gravity.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gravity : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float denom = transform.localPosition.magnitude;
+		//float magAcc = G * mass / (denom * denom);
+
+
+
+
+	}
+}
